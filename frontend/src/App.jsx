c1508f7f1766596bbe6c1b5586401d39c5db7542
Jessé Martins <jessemartins.programacao@gmail.com>
2026-01-29 14:22:36 -0300

    Update app routes with new sections pointing to UnderConstruction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,8 @@ import UserForm from './pages/Users/UserForm';
 
 import Settings from './pages/Settings';
 
+import UnderConstruction from './pages/UnderConstruction';
+
 function App() {
   return (
     <ConfigProvider>
@@ -25,6 +27,31 @@ function App() {
             <Route path="users/create" element={<UserForm />} />
             <Route path="users/edit/:id" element={<UserForm />} />
             <Route path="settings" element={<Settings />} />
+
+            {/* Pessoas */}
+            <Route path="clientes" element={<UnderConstruction />} />
+            <Route path="funcionarios" element={<UnderConstruction />} />
+            <Route path="fornecedores" element={<UnderConstruction />} />
+
+            {/* Cadastros */}
+            <Route path="formas-pgto" element={<UnderConstruction />} />
+            <Route path="frequencias" element={<UnderConstruction />} />
+            <Route path="cargos" element={<UnderConstruction />} />
+            <Route path="grupos" element={<UnderConstruction />} />
+            <Route path="acessos" element={<UnderConstruction />} />
+
+            {/* Financeiro */}
+            <Route path="receber" element={<UnderConstruction />} />
+            <Route path="despesas" element={<UnderConstruction />} />
+            <Route path="relatorio-financeiro" element={<UnderConstruction />} />
+            <Route path="rel-sintetico-despesas" element={<UnderConstruction />} />
+            <Route path="rel-sintetico-receber" element={<UnderConstruction />} />
+            <Route path="rel-balanco-anual" element={<UnderConstruction />} />
+            <Route path="rel-inadimplentes" element={<UnderConstruction />} />
+
+            {/* Outros */}
+            <Route path="caixas" element={<UnderConstruction />} />
+            <Route path="tarefas" element={<UnderConstruction />} />
           </Route>
         </Routes>
       </BrowserRouter>
